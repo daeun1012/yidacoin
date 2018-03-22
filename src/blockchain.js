d9a025e300768ab1e9bb2ee72d32b6d0d6c2add1
이다은 <daeun.lee@coinplug.com>
2018-03-22 12:12:08 +0900

    블럭 난이도 구현

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,12 +1,20 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+	hexToBinary = require("hex-to-binary");
+
+// 블럭 생성 주기 ( 시간 단위 s )
+const BLOCK_GENERATION_INTERVAL = 10 * 1000;
+// 난이도 조절 블럭 주기
+const DIFFICULTY_ADJUSMENT_INTERVAL = 10;
 
 class Block {
-	constructor(index, hash, previousHash, timestamp, data) {
+	constructor(index, hash, previousHash, timestamp, data, difficulty, nonce) {
 		this.index = index;
 		this.hash = hash;
 		this.previousHash = previousHash;
 		this.timestamp = timestamp;
 		this.data = data;
+		this.difficulty = difficulty;
+		this.nonce = nonce;
 	}
 }
 
@@ -16,7 +24,9 @@ const genesisBlock = new Block(
 	"E4FC1B06F01D2BACAFF97D534F284C0618897B64D4CB36EF28582A26A2C6921F",
 	null,
 	1521168884757,
-	"This is the genesis!!"
+	"This is the genesis!!",
+	0,
+	0
 );
 
 let blockchain = [genesisBlock];
@@ -31,7 +41,8 @@ const getBlockchain = () => blockchain;
 
 // 해시 생성
 // SHA256 이용. 비트코인도 SHA256 사용중
-const createHash  = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();
+const createHash  = (index, previousHash, timestamp, data, difficulty, nonce) =>
+	CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data) + difficulty + nonce).toString();
 
 // 새로운 블럭 생성
 const createNewBlock = data => {
@@ -41,29 +52,98 @@ const createNewBlock = data => {
 	const newBlockIndex = previousBlock.index + 1;
 	// 현재 timestamp
 	const newTimestamp = getTimestamp();
-	// 새로운 해시 생성
-	const newHash = createHash(
-		newBlockIndex,
-		previousBlock.hash,
-		newTimestamp,
-		data
-	);
+	// 난이도
+	const difficulty = findDifficulty();
 
 	// 새로운 블럭 생성
-	const newBlock = new Block(
+	const newBlock = findBlock(
 		newBlockIndex,
-		newHash,
 		previousBlock.hash,
 		newTimestamp,
-		data
+		data,
+		difficulty
 	);
 	addBlockToChain(newBlock);
 	require("./p2p").broadcastNewBlock();
 	return newBlock;
 };
 
+// 난이도 찾기
+const findDifficulty = () => {
+	const newestBlock = getNewestBlock();
+	if (
+		newestBlock.index % DIFFICULTY_ADJUSMENT_INTERVAL === 0 &&
+        newestBlock.index !== 0
+	) {
+		return calculateNewDifficulty(newestBlock, getBlockchain());
+	} else {
+		return newestBlock.difficulty;
+	}
+};
+
+// 난이도 계산하기
+const calculateNewDifficulty = (newestBlock, blockchain) => {
+	const lastCalculatedBlock =
+        blockchain[blockchain.length - DIFFICULTY_ADJUSMENT_INTERVAL];
+	const timeExpected =
+        BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSMENT_INTERVAL;
+	const timeTaken = newestBlock.timestamp - lastCalculatedBlock.timestamp;
+	if (timeTaken < timeExpected / 2) {
+		return lastCalculatedBlock.difficulty + 1;
+	} else if (timeTaken > timeExpected * 2) {
+		return lastCalculatedBlock.difficulty - 1;
+	} else {
+		return lastCalculatedBlock.difficulty;
+	}
+};
+
+// 난이도에 맞는 블럭 찾기
+const findBlock = (index, previousHash, timestamp, data, difficulty) => {
+	let nonce = 0;
+	while (true) {
+		console.log("Current nonce", nonce);
+		const hash = createHash(
+			index,
+			previousHash,
+			timestamp,
+			data,
+			difficulty,
+			nonce
+		);
+		if (hashMatchesDifficulty(hash, difficulty)) {
+			return new Block(
+				index,
+				hash,
+				previousHash,
+				timestamp,
+				data,
+				difficulty,
+				nonce
+			);
+		}
+		nonce++;
+	}
+};
+
+// 난이도에 맞는 해시값 찾기
+const hashMatchesDifficulty = (hash, difficulty = 0) => {
+	const hashInBinary = hexToBinary(hash);
+	const requiredZeros = "0".repeat(difficulty);
+	console.log("Trying difficulty:", difficulty, "with hash", hashInBinary);
+	return hashInBinary.startsWith(requiredZeros);
+};
+
 // 해당 블럭의 hash 를 생성
-const getBlockHash = block => createHash(block.index, block.previousHash, block.timestamp, block.data);
+const getBlockHash = block =>
+	createHash(block.index, block.previousHash, block.timestamp, block.data, block.difficulty, block.nonce);
+
+// timestamp 유효성 검사
+const isTimeStampValid = (newBlock, oldBlock) => {
+	return (
+		oldBlock.timestamp - 60 * 1000 < newBlock.timestamp &&
+        newBlock.timestamp - 60 * 1000 < getTimestamp()
+	);
+};
 
 // 새로 생성한 블럭의 contents 검사
 const isBlockValid = (candidateBlock, latestBlock) => {
@@ -83,6 +163,10 @@ const isBlockValid = (candidateBlock, latestBlock) => {
 	} else if(getBlockHash(candidateBlock) !== candidateBlock.hash) {
 		// 생성한 블럭의 해시값 불일치
 		console.log("The hash of this block is invalid");
+		return false;
+	} else if (!isTimeStampValid(candidateBlock, latestBlock)) {
+		// timestamp 유효성 에러
+		console.log("The timestamp of this block is dodgy");
 		return false;
 	}
 
@@ -124,10 +208,17 @@ const isChainVaild = candidateChain => {
 	return true;
 };
 
+// 난이도의 합
+const sumDifficulty = anyBlockchain =>
+	anyBlockchain
+		.map(block => block.difficulty)
+		.map(difficulty => Math.pow(2, difficulty))
+		.reduce((a, b) => a + b);
+
 // 검증이 끝난 체인을 받아들임
 const replaceChain = candidateChain => {
-	// chain 유효성 검사 && 새로운 체인의 길이가 더 길다면 (새로운 블럭이 추가되었다면)
-	if(isChainVaild(candidateChain) && candidateChain.length > getBlockchain().length) {
+	// chain 유효성 검사 && 난이도의 합이 더 큰 쪽으로 변경
+	if(isChainVaild(candidateChain) && sumDifficulty(candidateChain) > sumDifficulty(getBlockchain())) {
 		blockchain = candidateChain;
 		return true;
 	} else {
